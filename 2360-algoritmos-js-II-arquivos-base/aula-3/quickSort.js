eb3bc0714cbecc256c72cc7ab3d86c46c2d252cd
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-11-17 18:12:09 -0300

    correção de bug

--- 2360-algoritmos-js-II-arquivos-base/aula-3/quickSort.js
+++ 2360-algoritmos-js-II-arquivos-base/aula-3/quickSort.js
@@ -30,31 +30,29 @@ if (indiceAtual < direita) {
 
 
 function particiona(array,esquerda,direita) {
-       log
+console.log("array",array);
+console.log("esquerda e direita",esquerda,direita);
 let pivo =array[Math.floor((esquerda+direita)/2)];
 let atualEsquerda=esquerda;
 let atualDireita=direita;
 
 //para qunado a seta da esquerda p\ direita e a seta da direota pra esquerda 
-while (atualEsquerda <= atualDireita) {
+ while (atualEsquerda <= atualDireita) {
     while (array[atualEsquerda].preco < pivo.preco) {
        atualEsquerda++;
     }
     while (array[atualDireita].preco > pivo.preco) {
         atualDireita--;
     }
-
-if (atualEsquerda <=atualDireita) {
+    if (atualEsquerda <=atualDireita) {
     trocarLugar(array,atualEsquerda,atualDireita);
     atualEsquerda++;
     atualDireita--;
+    } 
 
-} 
+ return atualEsquerda;
+ }
 
-
-
-}
-return atualEsquerda;
 }
 
 console.log(quickSort(listaLivros,0,listaLivros.length-1));
