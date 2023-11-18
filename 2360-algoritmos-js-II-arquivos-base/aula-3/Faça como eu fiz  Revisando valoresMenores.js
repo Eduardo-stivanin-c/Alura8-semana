b44f47005817c1707d909ff374f18d879c2e2c85
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-11-18 09:20:12 -0300

    Algoritmos com JavaScript II: aprofundando em algoritmos de ordenação e busca
     Algoritmos com JavaScript II: aprofundando em algoritmos de ordenação e busca

---
+++ 2360-algoritmos-js-II-arquivos-base/aula-3/Faça como eu fiz  Revisando valoresMenores.js
@@ -0,0 +1,37 @@
+/*function divideNoPivo(array) {
+ let pivo = array[Math.floor(array.length / 2)];
+ encontraMenores(pivo, array);
+
+ return array;
+}*/
+
+
+/*
+function divideNoPivo(array) {
+    let pivo = array[Math.floor(array.length / 2)];
+    encontraMenores(pivo, array);
+    let valoresMenores = 0;
+   
+    for(let analisando = 0; analisando < array.length; analisando++) {
+      let atual = array[analisando];
+    }
+   
+    return array;
+   }
+*/
+
+   function divideNoPivo(array) {
+    let pivo = array[Math.floor(array.length / 2)];
+    encontraMenores(pivo, array);
+    let valoresMenores = 0;
+   
+    for(let analisando = 0; analisando < array.length; analisando++) {
+      let atual = array[analisando];
+      if(atual.preco <= pivo.preco && atual !== pivo) {
+        trocaLugar(array, analisando, valoresMenores)
+        valoresMenores++
+      }
+    }
+   
+    return array;
+   }
---
+++ 2360-algoritmos-js-II-arquivos-base/aula-4/busca.js
@@ -0,0 +1,34 @@
+//busca
+
+const listaLivros=require('./arrayOrdenado')
+//de valor ordenadel ista já ordenada
+function busca(array,de,ate,valorBuscado) {
+    console.log('de,ate ',de,ate    );
+
+ // se preço pesquisado não estiver na lista  retorne -1
+if (de > ate) {
+    return -1
+}
+
+//####################################################
+
+
+    const meio=Math.floor((de +ate)/2);
+const atual=array[meio];
+if (valorBuscado===atual.preco) {
+    return meio;
+}
+//se valorBuscado < atual.preço
+if (valorBuscado <atual.preco) {
+   return busca(array,de,meio -1,valorBuscado);
+    
+}
+if (valorBuscado>atual.preco) {
+    return busca(array,meio +1,ate ,valorBuscado)
+}
+
+
+}//se passar preço  não tiver um livro vocm aqule preço nãda aerro emve\z de retoornar indice
+console.log(busca(listaLivros,0,listaLivros.length-1,60));
+
+//erro de stack overfllow caundo trav a pro llop infiito
